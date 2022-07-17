3b78e1a9c8d5814f19c5b99e596d6e0aa0243257
shrey-Odinproject <sonishreyf1@gmail.com>
2022-07-17 19:33:13 +0530

    Create a full functioning kitten applicationwith restful routes, views validations, forms, error/flash messages etc.

--- app/controllers/kittens_controller.rb
+++ app/controllers/kittens_controller.rb
@@ -1,2 +1,55 @@
 class KittensController < ApplicationController
+  def index
+    @kittens = Kitten.all
+  end
+
+  def new
+    @kitten = Kitten.new
+  end
+
+  def create
+    @kitten = Kitten.new(kitten_params)
+
+    if @kitten.save
+      flash[:success] = 'Kitten created'
+      redirect_to @kitten
+    else
+      flash.now[:error] = ':( creation error'
+      render :new, status: :unprocessable_entity
+    end
+  end
+
+  def show
+    @kitten = Kitten.find(params[:id])
+  end
+
+  def edit
+    @kitten = Kitten.find(params[:id])
+  end
+
+  def update
+    @kitten = Kitten.find(params[:id])
+
+    if @kitten.update(kitten_params)
+      flash[:success] = 'Kitten updated'
+      redirect_to @kitten
+    else
+      flash.now[:error] = ':( update error'
+      render :edit, status: :unprocessable_entity
+    end
+  end
+
+  def destroy
+    @kitten = Kitten.find(params[:id])
+    @kitten.destroy
+
+    flash[:success] = 'Kitten destroyed'
+    redirect_to root_path, status: :see_other
+  end
+
+  private
+
+  def kitten_params
+    params.require(:kitten).permit(:name, :age, :cuteness, :softness)
+  end
 end
--- app/models/kitten.rb
+++ app/models/kitten.rb
@@ -1,2 +1,8 @@
 class Kitten < ApplicationRecord
+  validates :name, presence: true
+  validates :age, presence: true
+  validates :cuteness, presence: true, inclusion: { in: %w(low mid high),
+    message: "%{value} is not a valid cuteness level" }
+  validates :softness, presence: true,inclusion: { in: %w(low mid high),
+    message: "%{value} is not a valid softness level" }
 end
