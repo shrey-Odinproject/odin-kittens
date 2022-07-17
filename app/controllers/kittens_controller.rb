ba7b302b2bf0bb8ed517bc380ede3701502fed4c
shrey-Odinproject <sonishreyf1@gmail.com>
2022-07-18 00:35:13 +0530

    Setup json response for show and index using rest-client

--- app/controllers/kittens_controller.rb
+++ app/controllers/kittens_controller.rb
@@ -1,6 +1,12 @@
 class KittensController < ApplicationController
   def index
     @kittens = Kitten.all
+
+    respond_to do |format|
+      format.html # index.html.erb
+      format.json { render :json => @kittens }
+    end
+
   end
 
   def new
@@ -21,6 +27,11 @@ class KittensController < ApplicationController
 
   def show
     @kitten = Kitten.find(params[:id])
+
+    respond_to do |format|
+      format.html
+      format.json { render :json => @kitten }
+    end
   end
 
   def edit
